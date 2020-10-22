fc319b6fa1d0b0c3d90a13e308b38a6f1b075d9f
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2020-10-23 01:03:20 +0530

    navbar setup completed by dom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 let main = document.querySelector('#content');
-
-const next = () => {
+let section = main.appendChild(document.createElement('section'));
+const navbar = () => {
     let nav = main.appendChild(document.createElement('nav'));
     nav.setAttribute("class","navbar navbar-expand-lg navbar-light bg-light justify-content-between")
+    nav.appendChild(document.createElement('a')).setAttribute("class", "navbar-brand");
+    document.querySelector('.navbar-brand').innerHTML = "RESTURANT";
+    let nav_div = nav.appendChild(document.createElement('div'));
+    nav_div.innerHTML = `
+         <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact us</a>
+          </li>
+        </ul>
+    `
 }
 
-next()
+const home = () => {
+   
+
+}
+
+navbar()
 
 
